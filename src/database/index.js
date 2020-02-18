081d9c33d99b64c73f2343f222b57ca571a95cc7
BrasileiroDev <brasileirodev@gmail.com>
2020-02-18 11:02:07 -0300

    Finalização da estapa do Backend

--- src/database/index.js
+++ src/database/index.js
@@ -1,17 +1,31 @@
 import Sequelize from 'sequelize';
-import databaseConfig from '../config/database';
+import mongoose from 'mongoose';
+import databaseConfig from '../app/config/database';
 import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
-const models = [User];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+    });
   }
 }
 export default new Database();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './app/config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
+import ProviderController from './app/controllers/ProviderController';
+import Appointments from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -11,5 +21,14 @@ routes.post('/sessions', SessionController.store);
 // Rotas acessadas apenas com autenticação
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+routes.post('/appointments', Appointments.store);
+routes.get('/appointments', Appointments.index);
+routes.delete('/appointments/:id', Appointments.delete);
+routes.get('/schedule', ScheduleController.index);
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
